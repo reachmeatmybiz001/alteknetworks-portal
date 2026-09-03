6e3ebfd88aa650c03f805fd154925915b0684736
reachmeatmybiz001 <reachmeatmybiz001@gmail.com>
2026-09-04 00:25:42 +0530

    Add user administration service

--- src/userService.js
+++ src/userService.js
@@ -1,1 +1,24 @@
+import { createUser, deleteUser, listUsers, updateUser } from './api'
 
+export { listUsers, createUser, updateUser, deleteUser }
+
+export const USER_ROLES = [
+  'Customers',
+  'SupportAdmins',
+  'UserAdmins',
+  'SuperAdmins',
+]
+
+export function isCustomerRole(role) {
+  return role === 'Customers'
+}
+
+export function isAdministratorRole(role) {
+  return ['SupportAdmins', 'UserAdmins', 'SuperAdmins'].includes(role)
+}
+
+export function canManageUserRole(actorRole, targetRole) {
+  if (actorRole === 'SuperAdmins') return true
+  if (actorRole !== 'UserAdmins') return false
+  return targetRole === 'Customers'
+}
